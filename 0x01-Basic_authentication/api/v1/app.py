0e67aa3fd97169133ee3523c5a977a785f009c99
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-18 23:28:06 +0100

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -31,7 +31,7 @@ def before_request() -> str:
     if auth is None:
         return
     if not auth.require_auth(request.path, excluded_paths):
-        return 
+        return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
