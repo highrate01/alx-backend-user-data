2f89f35fc85e9420825398412a7ef741f5f36bf4
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-20 21:38:22 +0100

    before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -41,8 +41,8 @@ def before_request() -> str:
         return
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request):
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
