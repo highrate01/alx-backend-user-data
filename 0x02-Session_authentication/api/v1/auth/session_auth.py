f2cc50d0abb4b3d28980b76065085e1e15777bda
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-20 20:39:30 +0100

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+contains SessionAuth class module that inherits
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    authentication mechanism
+    """
+    pass
