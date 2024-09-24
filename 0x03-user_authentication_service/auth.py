3b04f251d068f06a52f8e1f86ad915a1a5057065
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-24 21:54:28 +0100

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,14 +8,14 @@ from user import User
 from sqlalchemy.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """
+"""def _hash_password(password: str) -> bytes:
+
     Hashes a password using bcrypt
-    """
+
     gen_salt = bcrypt.gensalt()
     hash_pwd = bcrypt.hashpw(password.encode('utf-8'), gen_salt)
 
-    return hash_pwd
+    return hash_pwd"""
 
 
 class Auth:
@@ -33,6 +33,15 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = _hash_password(password)
+            hashed_password = self._hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def _hash_password(self, password: str) -> bytes:
+        """
+        Hashes a password using bcrypt
+        """
+        gen_salt = bcrypt.gensalt()
+        hash_pwd = bcrypt.hashpw(password.encode('utf-8'), gen_salt)
+
+        return hash_pwd
