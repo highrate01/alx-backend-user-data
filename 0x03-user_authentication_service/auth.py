7a9e04fbb44bd008c9e0dc423a5dfb9bcfe79613
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-24 22:46:30 +0100

    reg

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,7 +16,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email, password):
+    def register_user(self, email: str, password: str) -> Optional[User]:
         """
         Registers user email
         """
