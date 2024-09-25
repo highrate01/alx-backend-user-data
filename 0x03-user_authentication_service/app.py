1965ee9ee9363462cdec1e0397e74fa6686bfa29
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-25 20:11:06 +0100

    register user end point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,38 @@
 """
 contains flask routes
 """
-from flask import Flask, jsonify
+from flask import (
+        Flask,
+        jsonify,
+        request
+        )
+from auth import Auth
+
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def welcome() -> str:
-    """return json payload"""
+    """welcome message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register() -> str:
+    """
+    register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        reg_user = AUTH.register_user(email, password)
+        return jsonify({"email": reg_user.email,
+                        "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
