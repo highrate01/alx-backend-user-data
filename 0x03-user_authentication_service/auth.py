ad80f0866c5d1ac6170818e63e351ff125ac2b90
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-25 16:41:33 +0100

    register

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,41 +1,36 @@
 #!/usr/bin/env python3
-"""
-contains a hashed password function
-"""
 import bcrypt
 from db import DB
 from user import User
-from typing import Optional
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-
-
-def _hash_password(password: str) -> bytes:
-    """
-    Hashes a password using bcrypt
-    """
-    gen_salt = bcrypt.gensalt()
-    hash_pwd = bcrypt.hashpw(password.encode('utf-8'), gen_salt)
-
-    return hash_pwd
+from typing import Optional
 
 
 class Auth:
     """Auth class to interact with the authentication database.
     """
-
     def __init__(self):
         self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """
+        Hashes a password using bcrypt
+        """
+        gen_salt = bcrypt.gensalt()
+        hash_pwd = bcrypt.hashpw(password.encode('utf-8'), gen_salt)
+        return hash_pwd
 
     def register_user(self, email: str, password: str) -> User:
         """
         Registers user email
         """
         try:
-            if self._db.find_user_by(email=email) is not None:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
                 raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = _hash_password(password)
+            hashed_password = self._hash_password(password)
             return self._db.add_user(email, hashed_password.decode('utf-8'))
         except InvalidRequestError:
             raise ValueError("Invalid email")
