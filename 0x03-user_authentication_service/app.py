cd1b27910485a8e18fbbf4de2aa5b26804e836af
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-25 16:29:09 +0100

    basic flask

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+contains flask routes
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+app.route('/')
+def payload():
+    """return json payload"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
