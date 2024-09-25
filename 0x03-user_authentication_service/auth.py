b8d8af4a033d0d1320604735d641736db7b58de3
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-25 15:54:24 +0100

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,12 +30,8 @@ class Auth:
         """
         Registers user email
         """
-        try:
-            existing_user = self._db.find_user_by(email=email)
-            if existing_user:
-                raise ValueError(f"User {email} already exists")
-            else:
-                pass
-        except NoResultFound:
-            hashed_password = _hash_password(password)
-            return self._db.add_user(email, hashed_password)
+        if self._db.find_user_by(email=email) is not None:
+            raise ValueError(f"User {email} already exists")
+
+        hashed_password = _hash_password(password)
+        return self._db.add_user(email, hashed_password)
